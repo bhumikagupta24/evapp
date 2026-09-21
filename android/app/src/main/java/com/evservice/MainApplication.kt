a1e80b0635e3dc1af1dd2e15647a4200958c92ad
Bhumika Gupta <minig2429@gmail.com>
2026-09-21 22:50:28 +0530

    fix: remove duplicate RNDateTimePickerPackage addition from MainApplication

--- android/app/src/main/java/com/evservice/MainApplication.kt
+++ android/app/src/main/java/com/evservice/MainApplication.kt
@@ -11,16 +11,13 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
-import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> {
-          val packages = PackageList(this).packages.toMutableList()
-          packages.add(RNDateTimePickerPackage())
-          return packages
+          return PackageList(this).packages
         }
 
         override fun getJSMainModuleName(): String = "index"
