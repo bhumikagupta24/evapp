8d3cdea78f006d105a416e1c1a0fd596d067a900
Bhumika Gupta <minig2429@gmail.com>
2026-09-21 22:42:39 +0530

    fix: pin datetimepicker 7.7.0 for RN 0.73 compatibility and register RNDateTimePickerPackage in MainApplication

--- android/app/src/main/java/com/evservice/MainApplication.kt
+++ android/app/src/main/java/com/evservice/MainApplication.kt
@@ -11,15 +11,16 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return PackageList(this).packages
+          val packages = PackageList(this).packages.toMutableList()
+          packages.add(RNDateTimePickerPackage())
+          return packages
         }
 
         override fun getJSMainModuleName(): String = "index"
